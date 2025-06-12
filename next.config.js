fbce6f072e1890a2913427d0afc7f4903ca4b7f2
MohamadHosein86-Dev <mohamadhoseinsafaryan86@gmail.com>
2025-06-12 11:51:36 +0330

    Deploy

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path')
+const path = require('path') // ❗ حتماً این خط باشه
 
 /** @type {import('next').NextConfig} */
-
-// Remove this if you're not using Fullcalendar features
-
 module.exports = {
   trailingSlash: true,
   reactStrictMode: false,
